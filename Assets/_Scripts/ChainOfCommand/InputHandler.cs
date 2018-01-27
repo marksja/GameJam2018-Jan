be712d3b7f74f2b138e0aa5b4981a13ba4196b9f
Becarls <becarls@umich.edu>
2018-01-27 15:25:30 -0500

    Modified script in the new repo to handle inputs

--- Assets/_Scripts/ChainOfCommand/InputHandler.cs
+++ Assets/_Scripts/ChainOfCommand/InputHandler.cs
@@ -3,40 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using InControl;
 
-public class InputHandler : MonoBehaviour {
-    [Range(0,1)] //There's two players, its hard coded, go cry about it
+public class InputHandler : MonoBehaviour
+{
+    [Range(0, 1)] //There's two players, its hard coded, go cry about it
     public int playerNumber;
     public int numberOfShips = 3;
     public CommandQueue orders;
 
     private bool controllersIn = false;
+    private bool needInput = false;
+    private int input;
     private int[] inputs;
     private InputDevice device;
-    
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start() {
         device = InputManager.Devices[playerNumber];
         inputs = new int[numberOfShips];
-        for(int i = 0; i < numberOfShips; i++) {
+        for (int i = 0; i < numberOfShips; i++) {
             inputs[i] = -1;
         }
         StartCoroutine("ChooseCommands");
     }
-	
-    int WaitForInput() {
-        while (true) {
+
+    void Update() {
+        while (needInput) {
             if (device.Action1) {
                 Debug.Log("Oy");
-                return 0;
+                input = 0;
+                needInput = false;
             }
             else if (device.Action2) {
                 Debug.Log("Oof");
-                return 1;
+                input = 1;
+                needInput = false;
             }
             else if (device.Action3) {
                 Debug.Log("Meh");
-                return 2;
+                input = 2;
+                needInput = false;
             }
+        }
+    }
+
+    IEnumerator WaitForInput() {
+        while (true) {
+            if (device.Action1) {
+                Debug.Log("Oy");
+                input = 0;
+                needInput = false;
+            }
+            else if (device.Action2) {
+                Debug.Log("Oof");
+                input = 1;
+                needInput = false;
+            }
+            else if (device.Action3) {
+                Debug.Log("Meh");
+                input = 2;
+                needInput = false;
+            }
+
+            yield return new WaitForEndOfFrame();
         }
     }
 
@@ -45,15 +73,24 @@ public class InputHandler : MonoBehaviour {
         CommandQueue.Command attackId;
         int targetId = -1;
 
+        //We go through all the ships we have and choose the ship Id, the attack the choose, and their target
         for (int i = 0; i < numberOfShips; i++) {
-            shipId = WaitForInput();
+            shipId = i;
+
             if (inputs[i] != 1) {
-                attackId = (CommandQueue.Command)WaitForInput();
+                needInput = true;
+                StartCoroutine("WaitForInput");
+                while (needInput) { yield return new WaitForEndOfFrame(); } //Blocking until we have input
+                attackId = (CommandQueue.Command)input;
             }
             else {
                 attackId = (CommandQueue.Command)3; //Hold if we Heavy attacked last turn
             }
-            targetId = WaitForInput();
+
+            needInput = true;
+            StartCoroutine("WaitForInput");
+            while (needInput) { yield return new WaitForEndOfFrame(); } //Blocking until we have input
+            targetId = input;
 
             orders.IssueCommand(shipId, attackId, targetId); //Give the order
             inputs[i] = (int)attackId; //Store the last attack 
