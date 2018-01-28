860e5f127e040424cb808ec36675b330025596f1
Cooper Riehl <cariehl@umich.edu>
2018-01-27 22:12:51 -0500

    Add script to display a message when player wins

---
+++ Assets/EndGameWatcher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameWatcher : MonoBehaviour {
+
+	public Text winText;
+
+	public Ship p1Destroyer;
+	public Ship p2Destroyer;
+
+	string winTextTemplate = "Player {0} wins!";
+
+	void Start() {
+		StartCoroutine(EndCheckLoop());
+	}
+
+	IEnumerator EndCheckLoop() {
+		while (true) {
+			if (p1Destroyer.alive && !p2Destroyer.alive) {
+				winText.text = string.Format(winTextTemplate, "1");
+				winText.gameObject.SetActive(true);
+				yield break;
+			}
+			else if (!p1Destroyer.alive && p2Destroyer.alive) {
+				winText.text = string.Format(winTextTemplate, "2");
+				winText.gameObject.SetActive(true);
+				yield break;
+			}
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+}
