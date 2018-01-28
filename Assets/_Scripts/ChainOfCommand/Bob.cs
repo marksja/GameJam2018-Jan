3db8a1f1d2b6e24ec9f3a4e5c55c607f4be08774
Becarls <becarls@umich.edu>
2018-01-28 05:00:51 -0500

    failed attempt at bobbing

---
+++ Assets/_Scripts/ChainOfCommand/Bob.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour
+{
+    bool run = true;
+    public float offset = 3f;
+    public float speed = 2f;
+    public float journeyLength = 4;
+    public UnityEngine.AnimationCurve animCurve;
+    private Vector3 start;
+    private float startTime;
+
+    void Start() {
+        startTime = Time.time;
+        start = this.transform.position;
+        StartCoroutine("StartBobbing");
+    }
+
+    //I long for the sweet embrace of death
+    IEnumerator StartBobbing() {
+        while (true) {
+            if (run) {
+                float distCovered = (Time.time - startTime) * speed;
+                float fracJourney = distCovered / journeyLength;
+                float new_y = Mathf.Lerp(start.y - offset, start.y + offset, animCurve.Evaluate(fracJourney));
+                Vector3 newPosition = new Vector3(start.x, new_y, start.z);
+                transform.position = newPosition;
+            }
+        }
+    }
+}
