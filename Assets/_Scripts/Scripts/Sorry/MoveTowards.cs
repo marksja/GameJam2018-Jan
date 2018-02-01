10e01241de8063933ed2a25174fdb6d362290d3d
Cooper Riehl <cariehl@umich.edu>
2018-02-01 09:55:47 -0500

    Revert "Removed some old scripts"
    This reverts commit 3f69fea513eaa5f83ef6727defaffa89319b8816.

---
+++ Assets/_Scripts/Scripts/Sorry/MoveTowards.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float angle;
+    public float speed;
+    public float aliveTime;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+        StartCoroutine(AliveTime());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(speed * Mathf.Cos(angle * Mathf.Deg2Rad), speed * Mathf.Sin(angle * Mathf.Deg2Rad));
+	}
+
+    IEnumerator AliveTime() {
+        yield return new WaitForSeconds(aliveTime);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/_Scripts/Scripts/SuspiciousNoiseResponder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SuspiciousNoiseResponder : MonoBehaviour {
+
+	public InvestigatePointEvent OnHearNoise;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		SuspiciousNoiseSource noiseSource = other.GetComponent<SuspiciousNoiseSource>();
+		if (noiseSource != null) {
+			OnHearNoise.Invoke(noiseSource.transform.position);
+		}
+	}
+}
